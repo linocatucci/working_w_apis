bf6e472243e65e7e95a48dbfdd2d89757a989daa
linocatucci <Lino@Fernie.local>
2017-07-12 20:29:17 +0200

    movie search app

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+// &apikey=ed6ba6e4 
+// s = search on title
+// http://www.omdbapi.com/?&apikey=ed6ba6e4&s=amsterdam
+
+app.get('/search', function (req, res) {
+    res.send('search page!')
+    //request('http:')
+})
+
+app.get('/results', function (req, res) {
+    res.send('results page!')
+    request('http://www.omdbapi.com/?&apikey=ed6ba6e4&s=amsterdam:', function (error, response, body) {
+
+    });
+});
+
+var port = 3000;
+app.listen(port, function () {
+    console.log('Move App has started!');
+})
